44971f8a4ba9e64c4e7b6e8349b377c99a28de88
Nate <7nater.nelson@gmail.com>
2021-03-03 17:15:20 -0700

    Add post /products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
         if session[:user_id]
             user = User.find_by(id: session[:user_id])
             @products = user.products
+            binding.pry
             erb :'/product/index'
         else
             redirect '/'
@@ -19,6 +20,28 @@ class UsersController < ApplicationController
     end
 
     post '/products' do 
-        binding.pry
+        if session[:user_id]
+            p = params[:product]
+            product = Product.new(name: p[:name], sku: p[:sku], description: p[:description], color: p[:color], company: p[:company])
+            if product.save
+                p[:item_requirements].each do |i|
+                    ir = ItemRequirement.new()
+                    ir.name = i[:name]
+                    ir.length = i[:length]
+                    ir.description = i[:description]
+                    ir.required = (i[:required] == "on")
+                    ir.length_required = (i[:length_required] == "on")
+                    ir.save
+                    product.item_requirements << ir
+                end
+                user = User.find_by(id: session[:user_id])
+                user.products << product
+                redirect '/products'
+            else 
+                redirect '/products/new'
+            end 
+        else
+            redirect '/'
+        end
     end
 end
